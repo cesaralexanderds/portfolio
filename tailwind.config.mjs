d827c90beaec231dc3dddf9ef2eddf9e4bd3996d
César Delgadillo Sanchez <cesardelgadillosanchez@MacBook-Air-de-Cesar.local>
2024-12-08 05:22:27 -0600

    change colors and finish projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+			  primary: '#17292b', 
+			  secondary: '#385756', 
+			  back: '#0b1719',
+			  hoverText: '#355f57',
+			},
+		  },	  
 	},
 	plugins: [],
 }
